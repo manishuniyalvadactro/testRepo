fb095d67e29e53a3848323343011751fb5520dd0
manishuniyalvadactro <39897716+manishuniyalvadactro@users.noreply.github.com>
2021-04-12 16:14:28 +0530

    changes done

--- custom.ts
+++ custom.ts
@@ -2,15 +2,16 @@
 /**
  * Stemrobo Icon
  */
-//% weight=80 color=#0fbc11 icon=""
+//% weight=280 color=#0fbc11 icon=""
 namespace Stemrobo {
     /**
      * Blink an led
-     * @param x horzontal led
+     * @param x horizontal led
      * @param y vertical coordinate 
-     * @param interval time in milli second between blink     */
-    //% block
-    export function blink(x:number,y:number,interval:number): void {
+     * @param interval time in milli second between blink    
+     */
+    //% block="blink x $x y $y every $interval ms"
+    export function blink(x: number, y: number, interval:number): void {
         let sprite = game.createSprite(x,y)
         sprite.setBlink(interval)
         sprite.blink()
