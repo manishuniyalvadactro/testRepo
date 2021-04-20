33d01baef9c803bbc28ce2ddf1e654842fb1b0fa
manishuniyalvadactro <39897716+manishuniyalvadactro@users.noreply.github.com>
2021-04-20 12:13:58 +0530

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -11,7 +11,7 @@ namespace Stemrobo {
      * @param interval time in milli second between blink    
      */
     //% block="blink x $x y $y every $interval ms"
-  
+    //% weight=280 color=#ffb3e6 
     export function blink(x: number, y: number, interval:number): void {
         let sprite = game.createSprite(x,y)
         sprite.setBlink(interval)
