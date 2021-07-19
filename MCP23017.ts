5bd142686014b1fdf5e4738f1df82b2f37fe7937
manishuniyalvadactro <39897716+manishuniyalvadactro@users.noreply.github.com>
2021-07-19 11:57:20 +0530

    Update MCP23017.ts

--- MCP23017.ts
+++ MCP23017.ts
@@ -49,7 +49,7 @@ let myMCP23017Address = ADDRESS.A20
 /**
  * Blocks
  */
-"//% weight=100 color=#0fbc12 icon="
+"//% weight=100 color=#ff5733 icon="
 namespace MCP23017 {
     //% block
     //% advanced = true
